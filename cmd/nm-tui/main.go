d08e5013a54934b6cf60cbd4f84b24cdea975546
alphameo <pavel.2014.play@gmail.com>
2026-01-04 13:17:27 +0300

    refactor(main): bump to new state

--- cmd/nm-tui/main.go
+++ cmd/nm-tui/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"github.com/alphameo/nm-tui/internal/infra"
 	"github.com/alphameo/nm-tui/internal/logger"
-	"github.com/alphameo/nm-tui/internal/ui"
+	"github.com/alphameo/nm-tui/internal/ui/views"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -11,7 +12,7 @@ func main() {
 	logger.Level = logger.ErrorsLvl
 	logger.Informln("The program is running")
 	defer logger.Informln("Program is closed")
-	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
+	p := tea.NewProgram(views.NewMainModel(infra.NewNMCLI()), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		logger.Errln(err)
 	}
