4676edc30351970dae7fa8ee770c3e432b044468
alphameo <pavel.2014.play@gmail.com>
2026-01-06 12:56:16 +0300

    refactor(main): change log path

--- cmd/nm-tui/main.go
+++ cmd/nm-tui/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/alphameo/nm-tui/internal/infra"
 	"github.com/alphameo/nm-tui/internal/logger"
 	"github.com/alphameo/nm-tui/internal/ui/views"
@@ -8,10 +12,21 @@ import (
 )
 
 func main() {
-	logger.FilePath("./log")
+	logPath := os.ExpandEnv("$HOME") + "/.cache/nm-tui/log"
+	_, err := os.Stat(logPath)
+	if errors.Is(err, os.ErrNotExist) {
+		_, err = os.Create(logPath)
+		fmt.Println(err)
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+
+	logger.FilePath(logPath)
 	logger.Level = logger.ErrorsLvl
 	logger.Informln("The program is running")
 	defer logger.Informln("Program is closed")
+
 	p := tea.NewProgram(views.NewMainModel(infra.NewNMCLI()), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		logger.Errln(err)
