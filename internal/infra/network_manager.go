e46331a58be4351a742418c70317607beee13069
alphameo <pavel.2014.play@gmail.com>
2026-01-11 21:42:24 +0300

    refactor(infra): reformat doc comments

--- internal/infra/network_manager.go
+++ internal/infra/network_manager.go
@@ -22,36 +22,36 @@ type WifiInfo struct {
 }
 
 type NetworkManager interface {
-	// GetAvailableWifi() shows list of wifi-networks able to be connected.
+	// GetAvailableWifi shows list of wifi-networks able to be connected.
 	GetAvailableWifi() ([]*WifiScanned, error)
 
-	// GetStoredWifi() shows list of stored connections and highlights the active one.
+	// GetStoredWifi shows list of stored connections and highlights the active one.
 	GetStoredWifi() ([]*WifiStored, error)
 
-	// ConnectWifi() connects to wifi-network with given ssid using given password.
+	// ConnectWifi connects to wifi-network with given ssid using given password.
 	ConnectWifi(ssid, password string) error
 
-	// ConnectStoredWifi() connects to wifi-network with given name if its password is saved.
+	// ConnectStoredWifi connects to wifi-network with given name if its password is saved.
 	ConnectStoredWifi(name string) error
 
-	// DisconnectFromWifi() disconnects from wifi-network with given name.
+	// DisconnectFromWifi disconnects from wifi-network with given name.
 	DisconnectFromWifi(name string) error
 
-	// GetConnectedWifi() gives table of saved connections.
+	// GetConnectedWifi gives table of saved connections.
 	GetConnectedWifi() ([]string, error)
 
-	// GetWifiPassword() gives password of saved wifi-network with given name.
+	// GetWifiPassword gives password of saved wifi-network with given name.
 	GetWifiPassword(name string) (string, error)
 
 	// GetWifiInfo gives information about saved wifi-network with given name.
 	GetWifiInfo(name string) (*WifiInfo, error)
 
-	// UpdateWifiInfo() updates information about wifi-network.
 	UpdateWifiInfo(info *WifiInfo) error
+	// UpdateWifiInfo updates information about wifi-network with given name.
 
-	// DeleteWifiConnection() removes wifi-network with given name from saved connections.
+	// DeleteWifiConnection removes wifi-network with given name from saved connections.
 	DeleteWifiConnection(name string) error
 
-	// ConnectVPN() connects to vpn with given vpnName.
+	// ConnectVPN connects to vpn with given vpnName.
 	ConnectVPN(vpnName string) error
 }
