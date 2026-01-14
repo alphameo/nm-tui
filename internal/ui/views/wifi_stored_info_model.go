c55f40f89f61e4006518b9faccbe2ddc87aaef47
alphameo <pavel.2014.play@gmail.com>
2026-01-14 13:28:44 +0300

    feat(ui/views/wifi_info): add View

--- internal/ui/views/wifi_stored_info_model.go
+++ internal/ui/views/wifi_stored_info_model.go
@@ -2,22 +2,54 @@ package views
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/alphameo/nm-tui/internal/infra"
+	"github.com/alphameo/nm-tui/internal/ui/styles"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type WifiStoredInfoModel struct {
-	ssid string
+	ssid                string
+	name                textinput.Model
+	password            textinput.Model
+	active              bool
+	autoconnect         bool
+	autoconnectPriority textinput.Model
+	focusIndex          int
 }
 
 func NewStoredInfoModel() *WifiStoredInfoModel {
-	return &WifiStoredInfoModel{}
+	n := textinput.New()
+	n.Width = 20
+	n.Prompt = ""
+	n.Focus()
+	n.Placeholder = "name"
+
+	p := textinput.New()
+	p.Width = 20
+	p.Prompt = ""
+	p.EchoMode = textinput.EchoPassword
+	p.EchoCharacter = '•'
+	p.Placeholder = "password"
+
+	ap := textinput.New()
+	ap.Width = 4
+	ap.Prompt = ""
+
+	return &WifiStoredInfoModel{name: n, password: p, autoconnectPriority: ap}
 }
 
-func (m *WifiStoredInfoModel) setNew(ssid string) {
-	m.ssid = ssid
+func (m *WifiStoredInfoModel) setNew(info *infra.WifiInfo) {
+	m.ssid = info.SSID
+	m.name.SetValue(info.Name)
+	m.password.SetValue(info.Password)
+	m.active = info.Active
+	m.autoconnect = info.Autoconnect
+	m.autoconnectPriority.SetValue(strconv.Itoa(info.AutoconnectPriority))
 }
 
 func (m WifiStoredInfoModel) Init() tea.Cmd {
@@ -36,7 +68,45 @@ func (m WifiStoredInfoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m WifiStoredInfoModel) View() string {
+	inputStyle := lipgloss.
+		NewStyle().
+		BorderStyle(styles.BorderStyle)
+
+	nameBlock := lipgloss.JoinHorizontal(lipgloss.Center, "Name", inputStyle.Render(m.name.View()))
+
+	passwordBlock := lipgloss.JoinHorizontal(lipgloss.Center, "Password ", inputStyle.Render(m.password.View()))
+	autoconPriorityView := lipgloss.JoinHorizontal(lipgloss.Center, "Autoconnect priority ", inputStyle.Render(m.autoconnectPriority.View()))
+
 	sb := strings.Builder{}
-	fmt.Fprintf(&sb, "%s\n", m.ssid)
+	fmt.Fprintf(
+		&sb,
+		"SSID: %s%s\n%s\n%s\nAutoconnect %s\n%s",
+		m.ssid,
+		m.connectionView(),
+		nameBlock,
+		passwordBlock,
+		checkboxView(m.active),
+		autoconPriorityView,
+	)
 	return sb.String()
 }
+
+func (m WifiStoredInfoModel) connectionView() string {
+	if m.active {
+		return " (connected)"
+	} else {
+		return ""
+	}
+}
+
+func checkboxView(value bool) string {
+	if value {
+		return "[⏺]"
+	} else {
+		return "[ ]"
+	}
+}
+
+// func (m WifiStoredInfoModel) focusNext() {
+// 	m.focusIndex = min(m.focusIndex + 1, m.)
+// }
