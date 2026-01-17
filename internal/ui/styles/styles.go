de418d8e41f57c0716c4b6b472c4bd07acfb59c8
alphameo <pavel.2014.play@gmail.com>
2026-01-17 23:43:33 +0300

    refactor(ui/styles): use pointers for common styling and avoiding unnecessary copying

--- internal/ui/styles/styles.go
+++ internal/ui/styles/styles.go
@@ -14,20 +14,20 @@ var (
 
 	DefaultStyle = lipgloss.NewStyle().Foreground(TextColor)
 
-	Border        = lipgloss.RoundedBorder()
-	BorderedStyle = DefaultStyle.Border(Border)
+	Border        = defaultBorder()
+	BorderedStyle = DefaultStyle.Border(*Border)
 
 	TableStyle = tableStyle()
 
-	TabTabBorderInactive      = tabBorderInactive(Border)
-	TabTabBorderActive        = tabBorderActive(Border)
-	TabScreenBorder           = tabbedViewBorder(Border)
-	TabScreenBorderStyle      = DefaultStyle.Border(TabScreenBorder)
-	TabTabBorderInactiveStyle = DefaultStyle.Border(TabTabBorderInactive, true).Padding(0, 1)
-	TabTabBorderActiveStyle   = TabTabBorderInactiveStyle.Border(TabTabBorderActive, true)
+	TabTabBorderInactive      = tabBorderInactive(*Border)
+	TabTabBorderActive        = tabBorderActive(*Border)
+	TabScreenBorder           = tabbedViewBorder(*Border)
+	TabScreenBorderStyle      = DefaultStyle.Border(*TabScreenBorder)
+	TabTabBorderInactiveStyle = DefaultStyle.Border(*TabTabBorderInactive, true).Padding(0, 1)
+	TabTabBorderActiveStyle   = DefaultStyle.Border(*TabTabBorderActive, true).Padding(0, 1)
 
 	OverlayStyle = DefaultStyle.
-			Border(Border).
+			Border(*Border).
 			Align(lipgloss.Center, lipgloss.Center).
 			Foreground(TextColor)
 )
@@ -46,23 +46,28 @@ func tableStyle() table.Styles {
 	return style
 }
 
-func tabBorderInactive(border lipgloss.Border) lipgloss.Border {
-	border.BottomLeft = border.MiddleBottom
-	border.BottomRight = border.MiddleBottom
-	return border
+func defaultBorder() *lipgloss.Border {
+	border := lipgloss.RoundedBorder()
+	return &border
 }
 
-func tabBorderActive(border lipgloss.Border) lipgloss.Border {
+func tabBorderInactive(border lipgloss.Border) *lipgloss.Border {
+	border.BottomLeft = border.MiddleBottom
+	border.BottomRight = border.MiddleBottom
+	return &border
+}
+
+func tabBorderActive(border lipgloss.Border) *lipgloss.Border {
 	lBot := border.BottomRight
 	border.BottomRight = border.BottomLeft
 	border.BottomLeft = lBot
 	border.Bottom = " "
-	return border
+	return &border
 }
 
-func tabbedViewBorder(border lipgloss.Border) lipgloss.Border {
+func tabbedViewBorder(border lipgloss.Border) *lipgloss.Border {
 	border.Top = " "
 	border.TopLeft = border.Left
 	border.TopRight = border.Right
-	return border
+	return &border
 }
