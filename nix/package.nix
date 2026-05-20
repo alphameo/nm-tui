ba363b23bb34673282f18d1e00432f2124589c33
shimeoki <shimeoki@gmail.com>
2026-05-20 16:02:04 +0300

    build: nix package and devshell

---
+++ nix/package.nix
@@ -0,0 +1,28 @@
+{
+    src, # flakelight
+
+    lib,
+    buildGoModule,
+    makeWrapper,
+
+    networkmanager,
+}:
+buildGoModule {
+    inherit src;
+    pname = "nm-tui";
+    version = "0.0.1-unstable-2026-05-16";
+
+    vendorHash = "sha256-JYO6UHZwOmudADOiTAPM+mSm3KMKOERFTiM1beMN+MI=";
+
+    nativeBuildInputs = [ makeWrapper ];
+    subPackages = [ "cmd/nm-tui" ];
+
+    env = {
+        CGO_ENABLED = 0;
+    };
+
+    postInstall = ''
+        wrapProgram $out/bin/nm-tui \
+            --prefix PATH : ${lib.makeBinPath [ networkmanager ]}
+    '';
+}
