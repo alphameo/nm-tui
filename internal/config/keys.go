80f28b4084fc509ceeb2b1c4507a3a4572d79a06
alphameo <pavel.2014.play@gmail.com>
2026-07-22 12:12:58 +0300

    refactor(config): rewrite keys

--- internal/config/keys.go
+++ internal/config/keys.go
@@ -1,104 +1,91 @@
 package config
 
-import "strings"
-
-type Keys struct {
-	Main           MainKeys           `kdl:"main"`
-	Popup          PopupKeys          `kdl:"popup"`
-	Tabs           TabsKeys           `kdl:"tabs"`
-	Toggle         ToggleKeys         `kdl:"toggle"`
-	Wifi           WifiKeys           `kdl:"wifi"`
-	WifiAvailable  WifiAvailableKeys  `kdl:"wifi_available"`
-	WifiSaved      WifiSavedKeys      `kdl:"wifi_saved"`
-	Networking     NetworkingKeys     `kdl:"networking"`
-	Connector      ConnectorKeys      `kdl:"connector"`
-	ProfileCreator ProfileCreatorKeys `kdl:"profile_creator"`
-	ProfileEditor  ProfileEditorKeys  `kdl:"profile_editor"`
-	HotspotCreator HotspotCreatorKeys `kdl:"hotspot_creator"`
-}
-
-func DefaultKeys() *Keys {
-	return &Keys{}
-}
+import (
+	"strings"
+)
 
 type KeyBinding struct {
 	Keys []string `kdl:"keys,arguments"`
 }
 
+type Keys struct {
+	Toggle *KeyBinding `kdl:"toggle"`
+
+	Main   *MainKeys   `kdl:"main"`
+	Dialog *DialogKeys `kdl:"dialog"`
+
+	Wifi          *WifiKeys          `kdl:"wifi"`
+	WifiAvailable *WifiAvailableKeys `kdl:"wifi_available"`
+	WifiSaved     *WifiSavedKeys     `kdl:"wifi_saved"`
+}
+
+func DefaultKeys() *Keys {
+	return &Keys{
+		Toggle: &KeyBinding{Keys: []string{"space"}},
+		Main: &MainKeys{
+			NextTab:   &KeyBinding{Keys: []string{"]"}},
+			PrevTab:   &KeyBinding{Keys: []string{"["}},
+			FocusNext: &KeyBinding{Keys: []string{"tab"}},
+			FocusPrev: &KeyBinding{Keys: []string{"shift+tab"}},
+			Quit:      &KeyBinding{Keys: []string{"esc", "ctrl+c", "q", "ctrl+q"}},
+		},
+		Dialog: &DialogKeys{
+			FocusDown:          &KeyBinding{Keys: []string{"ctrl+j"}},
+			FocusUp:            &KeyBinding{Keys: []string{"tab"}},
+			TogglePWVisibility: &KeyBinding{Keys: []string{"ctrl+p"}},
+			Accept:             &KeyBinding{Keys: []string{"ctrl+enter"}},
+			Close:              &KeyBinding{Keys: []string{"ctrl+q"}},
+		},
+		Wifi: &WifiKeys{
+			CreateProfile:     &KeyBinding{Keys: []string{"a", "c"}},
+			OpenCaptivePortal: &KeyBinding{Keys: []string{"l"}},
+			EnableHotspot:     &KeyBinding{Keys: []string{"ctrl+h"}},
+			CreateHotspot:     &KeyBinding{Keys: []string{"h"}},
+		},
+		WifiAvailable: &WifiAvailableKeys{
+			Connect: &KeyBinding{Keys: []string{"enter"}},
+		},
+		WifiSaved: &WifiSavedKeys{
+			Edit:       &KeyBinding{Keys: []string{"enter"}},
+			Connect:    &KeyBinding{Keys: []string{"space"}},
+			Disconnect: &KeyBinding{Keys: []string{"ctrl+space"}},
+			Delete:     &KeyBinding{Keys: []string{"d", "delete"}},
+		},
+	}
+}
+
 type MainKeys struct {
-	Quit KeyBinding `kdl:"quit"`
+	NextTab   *KeyBinding `kdl:"next_tab"`
+	PrevTab   *KeyBinding `kdl:"prev_tab"`
+	FocusNext *KeyBinding `kdl:"focus_next"`
+	FocusPrev *KeyBinding `kdl:"focus_prev"`
+	Quit      *KeyBinding `kdl:"quit"`
 }
 
-type PopupKeys struct {
-	Close KeyBinding `kdl:"close"`
-}
-
-type TabsKeys struct {
-	Next KeyBinding `kdl:"next"`
-	Prev KeyBinding `kdl:"prev"`
-}
-
-type ToggleKeys struct {
-	Toggle KeyBinding `kdl:"toggle"`
+type DialogKeys struct {
+	FocusDown          *KeyBinding `kdl:"focus_down"`
+	FocusUp            *KeyBinding `kdl:"focus_up"`
+	TogglePWVisibility *KeyBinding `kdl:"toggle_pw_visibility"`
+	Accept             *KeyBinding `kdl:"accept"`
+	Close              *KeyBinding `kdl:"close"`
 }
 
 type WifiKeys struct {
-	NextWindow        KeyBinding `kdl:"next_window"`
-	FirstWindow       KeyBinding `kdl:"first_window"`
-	SecondWindow      KeyBinding `kdl:"second_window"`
-	Rescan            KeyBinding `kdl:"rescan"`
-	Create            KeyBinding `kdl:"create"`
-	OpenCaptivePortal KeyBinding `kdl:"open_captive_portal"`
-	EnableHotspot     KeyBinding `kdl:"enable_hotspot"`
-	CreateHotspot     KeyBinding `kdl:"create_hotspot"`
+	CreateProfile     *KeyBinding `kdl:"create_profile"`
+	OpenCaptivePortal *KeyBinding `kdl:"open_network_login"`
+	EnableHotspot     *KeyBinding `kdl:"enable_hotspot"`
+	CreateHotspot     *KeyBinding `kdl:"create_hotspot"`
 }
 
 type WifiAvailableKeys struct {
-	Rescan  KeyBinding `kdl:"rescan"`
-	Connect KeyBinding `kdl:"connect"`
+	Connect *KeyBinding `kdl:"connect"`
 }
 
 type WifiSavedKeys struct {
-	Edit       KeyBinding `kdl:"edit"`
-	Connect    KeyBinding `kdl:"connect"`
-	Disconnect KeyBinding `kdl:"disconnect"`
-	Rescan     KeyBinding `kdl:"rescan"`
-	Delete     KeyBinding `kdl:"delete"`
-}
-
-type NetworkingKeys struct {
-	Up     KeyBinding `kdl:"up"`
-	Down   KeyBinding `kdl:"down"`
-	Toggle KeyBinding `kdl:"toggle"`
-	Rescan KeyBinding `kdl:"rescan"`
-}
-
-type ConnectorKeys struct {
-	TogglePWVisibility KeyBinding `kdl:"toggle_pw_visibility"`
-	Up                 KeyBinding `kdl:"up"`
-	Down               KeyBinding `kdl:"down"`
-	Connect            KeyBinding `kdl:"connect"`
-}
-
-type ProfileCreatorKeys struct {
-	TogglePWVisibility KeyBinding `kdl:"toggle_pw_visibility"`
-	Up                 KeyBinding `kdl:"up"`
-	Down               KeyBinding `kdl:"down"`
-	Create             KeyBinding `kdl:"create"`
-}
-
-type ProfileEditorKeys struct {
-	TogglePWVisibility KeyBinding `kdl:"toggle_pw_visibility"`
-	Up                 KeyBinding `kdl:"up"`
-	Down               KeyBinding `kdl:"down"`
-	Save               KeyBinding `kdl:"save"`
-}
-
-type HotspotCreatorKeys struct {
-	TogglePWVisibility KeyBinding `kdl:"toggle_pw_visibility"`
-	Up                 KeyBinding `kdl:"up"`
-	Down               KeyBinding `kdl:"down"`
-	Create             KeyBinding `kdl:"create"`
+	Edit       *KeyBinding `kdl:"edit"`
+	Connect    *KeyBinding `kdl:"connect"`
+	Disconnect *KeyBinding `kdl:"disconnect"`
+	Delete     *KeyBinding `kdl:"delete"`
 }
 
 func HelpFromKeys(keys []string) string {
