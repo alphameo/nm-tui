e645ed8e65533096e58323d850c153b8ca24d044
alphameo <pavel.2014.play@gmail.com>
2026-08-07 15:43:04 +0300

    build: upd version in nix

--- nix/package.nix
+++ nix/package.nix
@@ -10,7 +10,7 @@
 buildGoModule {
     inherit src;
     pname = "nm-tui";
-    version = "0.0.3";
+    version = "0.1.0";
 
     vendorHash = "sha256-JYO6UHZwOmudADOiTAPM+mSm3KMKOERFTiM1beMN+MI=";
 
