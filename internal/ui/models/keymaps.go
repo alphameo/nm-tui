4903db253f2d918106bcfaef5e2f37957dfd7f1d
alphameo <pavel.2014.play@gmail.com>
2026-08-13 13:07:25 +0300

    refactor(models): revert keymaps desc

--- internal/ui/models/keymaps.go
+++ internal/ui/models/keymaps.go
@@ -47,71 +47,74 @@ func (k *keyMaps) FullHelp() [][]key.Binding {
 func initKeys(keys config.KeyConfig) keyMaps {
 	return keyMaps{
 		main: mainKeyMap{
-			quit:       NewKey(*keys.Main.Quit),
-			closePopup: NewKey(*keys.Dialog.Close),
+			quit:       NewKey(*keys.Main.Quit, "quit"),
+			closePopup: NewKey(*keys.Dialog.Close, "close popup"),
 		},
 		tabs: tabview.KeyMap{
-			Next: NewKey(*keys.Main.TabNext),
-			Prev: NewKey(*keys.Main.TabPrev),
+			Next: NewKey(*keys.Main.TabNext, "next tab"),
+			Prev: NewKey(*keys.Main.TabPrev, "prev tab"),
 		},
 		toggle: toggle.KeyMap{
-			Toggle: NewKey(*keys.Toggle),
+			Toggle: NewKey(*keys.Toggle, "toggle"),
 		},
 		connectivity: connectivityKeyMap{
-			prev:   NewKey(*keys.FocusPrev),
-			next:   NewKey(*keys.FocusNext),
-			rescan: NewKey(*keys.Rescan),
-			toggle: NewKey(*keys.Toggle),
+			prev:   NewKey(*keys.FocusPrev, "prev field"),
+			next:   NewKey(*keys.FocusNext, "next field"),
+			rescan: NewKey(*keys.Rescan, "rescan"),
+			toggle: NewKey(*keys.Toggle, ""),
 		},
 		networks: networksKeyMap{
-			winNext:           NewKey(*keys.FocusNext),
-			winPrev:           NewKey(*keys.FocusPrev),
-			rescan:            NewKey(*keys.Rescan),
-			createProfile:     NewKey(*keys.Networks.CreateProfile),
-			createHotspot:     NewKey(*keys.Networks.CreateHotspot),
-			quickHotspot:      NewKey(*keys.Networks.QuickHotspot),
-			openCaptivePortal: NewKey(*keys.Networks.OpenCaptivePortal),
-			win1:              NewKey(*keys.Focus1),
-			win2:              NewKey(*keys.Focus2),
+			winNext:           NewKey(*keys.FocusNext, "next window"),
+			winPrev:           NewKey(*keys.FocusPrev, "prev window"),
+			rescan:            NewKey(*keys.Rescan, "rescan all"),
+			createProfile:     NewKey(*keys.Networks.CreateProfile, "create profile"),
+			createHotspot:     NewKey(*keys.Networks.CreateHotspot, "create hotspot"),
+			quickHotspot:      NewKey(*keys.Networks.QuickHotspot, "quick hotspot"),
+			openCaptivePortal: NewKey(*keys.Networks.OpenCaptivePortal, "open login portal"),
+			win1:              NewKey(*keys.Focus1, "1st window"),
+			win2:              NewKey(*keys.Focus2, "2nd window"),
 		},
 		networkProfiles: networkProfilesKeyMap{
-			rescan:     NewKey(*keys.RescanFocused),
-			edit:       NewKey(*keys.NetworkProfiles.Edit),
-			connect:    NewKey(*keys.NetworkProfiles.Connect),
-			disconnect: NewKey(*keys.NetworkProfiles.Disconnect),
-			delete:     NewKey(*keys.NetworkProfiles.Delete),
+			rescan:     NewKey(*keys.RescanFocused, "rescan profiles"),
+			edit:       NewKey(*keys.NetworkProfiles.Edit, "edit"),
+			connect:    NewKey(*keys.NetworkProfiles.Connect, "connect"),
+			disconnect: NewKey(*keys.NetworkProfiles.Disconnect, "disconnect"),
+			delete:     NewKey(*keys.NetworkProfiles.Delete, "delete"),
 		},
 		availableNetworks: availableNetworksKeyMap{
-			rescan:  NewKey(*keys.RescanFocused),
-			connect: NewKey(*keys.AvailableNetworks.Connect),
+			rescan:  NewKey(*keys.RescanFocused, "rescan available"),
+			connect: NewKey(*keys.AvailableNetworks.Connect, "connect"),
 		},
 		profileEditor: profileEditorKeyMap{
-			prev:               NewKey(*keys.FocusPrev),
-			next:               NewKey(*keys.FocusNext),
-			save:               NewKey(*keys.Dialog.Accept),
-			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility),
+			prev:               NewKey(*keys.FocusPrev, "prev field"),
+			next:               NewKey(*keys.FocusNext, "next field"),
+			save:               NewKey(*keys.Dialog.Accept, "save"),
+			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility, "pw visibility"),
 		},
 		connector: connectorKeyMap{
-			prev:               NewKey(*keys.FocusPrev),
-			next:               NewKey(*keys.FocusNext),
-			connect:            NewKey(*keys.Dialog.Accept),
-			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility),
+			prev:               NewKey(*keys.FocusPrev, "prev field"),
+			next:               NewKey(*keys.FocusNext, "next field"),
+			connect:            NewKey(*keys.Dialog.Accept, "connect"),
+			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility, "pw visibility"),
 		},
 		profileCreator: profileCreatorKeyMap{
-			prev:               NewKey(*keys.FocusPrev),
-			next:               NewKey(*keys.FocusNext),
-			create:             NewKey(*keys.Dialog.Accept),
-			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility),
+			prev:               NewKey(*keys.FocusPrev, "prev field"),
+			next:               NewKey(*keys.FocusNext, "next field"),
+			create:             NewKey(*keys.Dialog.Accept, "create"),
+			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility, "pw visibility"),
 		},
 		hotspotCreator: hotspotCreatorKeyMap{
-			prev:               NewKey(*keys.FocusPrev),
-			next:               NewKey(*keys.FocusNext),
-			create:             NewKey(*keys.Dialog.Accept),
-			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility),
+			prev:               NewKey(*keys.FocusPrev, "prev field"),
+			next:               NewKey(*keys.FocusNext, "next field"),
+			create:             NewKey(*keys.Dialog.Accept, "create"),
+			togglePWVisibility: NewKey(*keys.Dialog.TogglePWVisibility, "pw visibility"),
 		},
 	}
 }
 
-func NewKey(keys []string) key.Binding {
-	return key.NewBinding(key.WithKeys(keys...))
+func NewKey(keys []string, desc string) key.Binding {
+	return key.NewBinding(
+		key.WithKeys(keys...),
+		key.WithHelp(HelpFromKeys(keys...), desc),
+	)
 }
