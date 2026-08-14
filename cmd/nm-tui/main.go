2cab1892a495250b488256802d585f5bc69d8dc7
alphameo <pavel.2014.play@gmail.com>
2026-08-14 20:55:41 +0300

    chore: print version in stdout instead of foggy Print

--- cmd/nm-tui/main.go
+++ cmd/nm-tui/main.go
@@ -20,7 +20,7 @@ var version = "dev"
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Printf("nm-tui %s\n", version)
+		fmt.Fprintf(os.Stdout, "nm-tui %s\n", version)
 		os.Exit(0)
 	}
 
