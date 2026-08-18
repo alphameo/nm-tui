be024f5a0fb59d921845b4b9c40a24421a3cd086
alphameo <pavel.2014.play@gmail.com>
2026-08-18 10:57:38 +0300

    chore: bump new version in nix

--- nix/package.nix
+++ nix/package.nix
@@ -10,7 +10,7 @@
 buildGoModule {
     inherit src;
     pname = "nm-tui";
-    version = "0.1.0";
+    version = "0.2.1";
 
     vendorHash = "sha256-JYO6UHZwOmudADOiTAPM+mSm3KMKOERFTiM1beMN+MI=";
 
