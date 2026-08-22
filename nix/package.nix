798f590867e5b68a5166803af55361c4e9c99292
shimeoki <shimeoki@gmail.com>
2026-08-22 16:40:07 +0300

    fix(nix): update vendorHash

--- nix/package.nix
+++ nix/package.nix
@@ -12,7 +12,7 @@ buildGoModule {
     pname = "nm-tui";
     version = "0.2.1";
 
-    vendorHash = "sha256-JYO6UHZwOmudADOiTAPM+mSm3KMKOERFTiM1beMN+MI=";
+    vendorHash = "sha256-AyYJFuuURz+QDe69iAgWlN1Xd7+Ofh4hVL0Xya706N8=";
 
     nativeBuildInputs = [ makeWrapper ];
     subPackages = [ "cmd/nm-tui" ];
