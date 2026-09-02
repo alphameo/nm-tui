828e944c7b295fcb8f335def2f774af8083693bc
shimeoki <79855381+shimeoki@users.noreply.github.com>
2026-09-02 12:37:22 +0300

    build(nix): chase version and simplify package (#37)
    * build(nix): remove networkmanager dependency
    
    in nixpkgs, for applications like lazygit and
    impala i didn't see iwd and git used as
    dependencies
    
    * chore(nix): update to 0.3.0
    
    also remove clunky let..in via finalAttrs

--- nix/package.nix
+++ nix/package.nix
@@ -1,33 +1,19 @@
 {
     src, # flakelight
-
-    lib,
     buildGo127Module,
-    makeWrapper,
-
-    networkmanager,
 }:
-let
-    version = "0.2.3";
-in
-buildGo127Module {
-    inherit src version;
+buildGo127Module (finalAttrs: {
+    inherit src;
     pname = "nm-tui";
+    version = "0.3.0";
 
     vendorHash = "sha256-qq7dbswbq+5h1iKBpohZBGEln+SsuZFnXs0RDYDhUy4=";
-
-    nativeBuildInputs = [ makeWrapper ];
 
     env = {
         CGO_ENABLED = 0;
     };
 
     ldflags = [
-        "-X main.version=${version}-nix"
+        "-X main.version=${finalAttrs.version}-nix"
     ];
-
-    postInstall = ''
-        wrapProgram $out/bin/nm-tui \
-            --prefix PATH : ${lib.makeBinPath [ networkmanager ]}
-    '';
-}
+})
