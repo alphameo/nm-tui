3f306beaa3c924d4196e206511765f84a04f5105
shimeoki <shimeoki@gmail.com>
2026-09-02 09:30:19 +0300

    fix: inject main.version
    also includes the version in nix package (but
    without the commit hash)

--- nix/package.nix
+++ nix/package.nix
@@ -7,10 +7,12 @@
 
     networkmanager,
 }:
-buildGo127Module {
-    inherit src;
-    pname = "nm-tui";
+let
     version = "0.2.3";
+in
+buildGo127Module {
+    inherit src version;
+    pname = "nm-tui";
 
     vendorHash = "sha256-qq7dbswbq+5h1iKBpohZBGEln+SsuZFnXs0RDYDhUy4=";
 
@@ -19,6 +21,10 @@ buildGo127Module {
     env = {
         CGO_ENABLED = 0;
     };
+
+    ldflags = [
+        "-X main.version=${version}-nix"
+    ];
 
     postInstall = ''
         wrapProgram $out/bin/nm-tui \
